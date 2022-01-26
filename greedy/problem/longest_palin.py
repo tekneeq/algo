f24a3b3a0609a8c41be6b5e94da85e37299a97f3
Eugene Park <eugenepark3@gmail.com>
2022-01-26 10:40:54 -0500

    greedy longest_palin

--- greedy/problem/longest_palin.py
+++ greedy/problem/longest_palin.py
@@ -1,2 +1,26 @@
-def longest_palindrome(self, s: str) -> int:
-    return 0
+"""
+Given a string s which consists of lowercase or uppercase letters,
+return the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+"""
+import collections
+
+
+def longest_palindrome(s: str) -> int:
+    ans = 0
+    uniq_char = False
+    for k, v in collections.Counter(s).items():
+        print(f"letter {k} value {v}")
+        ans += v // 2 * 2
+
+        if v % 2 == 1:
+            uniq_char = True
+
+    if uniq_char:
+        ans += 1
+
+    return ans
+
+
+print(longest_palindrome("hi"))
--- greedy/test/test_longest_palin.py
+++ greedy/test/test_longest_palin.py
@@ -1,2 +1,7 @@
+from greedy.problem.longest_palin import longest_palindrome
+
+
 def test_longest_palindrome():
-    assert False
+    assert longest_palindrome("abccccdd") == 7
+    assert longest_palindrome("a") == 1
+    assert longest_palindrome("bb") == 2
